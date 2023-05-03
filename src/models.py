a78b1683915c142b5957808ee8366891162eaaae
Borjamese <119245691+Borjamese@users.noreply.github.com>
2023-05-03 18:00:15 +0000

    end

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ from eralchemy2 import render_er
 Base = declarative_base()
 
 
-class User (Base):
+class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
@@ -17,21 +17,28 @@ class User (Base):
     password = Column(String(200),  nullable=False)
 
 
-class Planets (Base):
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    eye_color = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
 
+
+class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     rotation_period = Column(Integer, nullable=False)
 
 
-class Favorites (Base):
-
+class Favorites(Base):
     __tablename__ = 'favorites'
-
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
     planet_id = Column(Integer, ForeignKey('planets.id'))
+    people_id = Column(Integer, ForeignKey('people.id'))
+
 
     def to_dict(self):
         return {}
